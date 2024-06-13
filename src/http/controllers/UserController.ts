e1a2fc401d5b5178d8c25bf3c613a3a01770751a
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-06-13 06:03:25 -0300

    chore: check if the user's email is in use when creating the user

--- src/http/controllers/UserController.ts
+++ src/http/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import { createUser } from "../services/createUser";
+import { PrismaClient } from "@prisma/client";
 
 const createUserSchema = z.object({
   email: z.string().email(),
@@ -10,14 +11,25 @@ const createUserSchema = z.object({
 
 export type CreateUser = z.infer<typeof createUserSchema>;
 
+const prisma = new PrismaClient();
+
 export default {
   create: async (req: Request, res: Response) => {
     const { email, password, name } = createUserSchema.parse(req.body);
+    const existsUserEmail = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existsUserEmail) {
+      return res.status(400).send("E-mail already exists");
+    }
+
     try {
       await createUser({ email, password, name });
       return res.status(201).send();
     } catch (error) {
       console.error(error);
+      return res.status(500).send();
     }
   },
 };
